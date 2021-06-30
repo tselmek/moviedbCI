9108a4354cfed6196a7781e5a0f1d9a687928d3f
Clément Le Biez <clement@marmelab.com>
2021-06-30 10:15:00 +0200

    fix ci

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,11 +1,10 @@
 module.exports = {
     ci: {
-      collect: {
-        url: ['http://localhost:3000/'],
-        startServerCommand: 'yarn start',
-      },
-      upload: {
-        target: 'temporary-public-storage',
-      },
+        collect: {
+            url: ['http://localhost:3000/'],
+        },
+        upload: {
+            target: 'temporary-public-storage',
+        },
     },
-  };
+};
